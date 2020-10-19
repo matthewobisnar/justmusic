e9e8d15a2ef520fe53bcb543966052c8c1ac3a75
matthew bisnar <matthewobisnar@gmail.com>
2020-10-20 02:25:49 +0800

    .

--- src/Store/playlist.js
+++ src/Store/playlist.js
@@ -41,78 +41,78 @@ export default {
             duration:"5:01", 
             path: 'https://archive.org/download/shawnmendesunderstandaudio/Shawn%20Mendes%20-%20Understand%20%28audio%29.mp3'
           },
-          { 
-            id: 'ia601407',
-            image: "http://www.fouroverfour.jukely.com/wp-content/uploads/2017/01/Leroy-Sanchez.jpg", 
-            title:"Never Really Over", 
-            artist: "Cover by Leroy Sanchez", 
-            album: "Leroy Sanchez", 
-            duration:"4:29", 
-            path: 'https://ia601407.us.archive.org/11/items/katyperryneverreallyovercoverbyleroysanchez/KATY%20PERRY%20-%20Never%20Really%20Over%20%28Cover%20by%20Leroy%20Sanchez%29.mp3'
-          },
-          { 
-            id:'ia601408',
-            image: "http://www.fouroverfour.jukely.com/wp-content/uploads/2017/01/Leroy-Sanchez.jpg", 
-            title:"If I Can't Have You", 
-            artist: "Cover by Leroy Sanchez", 
-            album: "Leroy Sanchez", 
-            duration:"3:50", 
-            path: 'https://ia601507.us.archive.org/2/items/shawnmendesificanthaveyoucoverbyleroysanchez/SHAWN%20MENDES%20-%20If%20I%20Can%27t%20Have%20You%20%28Cover%20by%20Leroy%20Sanchez%29.mp3'
-          },
-          { 
-            id: 'ia801504',
-            image: "https://i1.sndcdn.com/avatars-000520361715-4dr7nr-t500x500.jpg", 
-            title:"Love Someone", 
-            artist: "Cover by Leroy Sanchez", 
-            album: "Leroy Sanchez", 
-            duration:"3:08", 
-            path: 'https://ia801504.us.archive.org/10/items/lukasgrahamlovesomeonecoverbyleroysanchez/Lukas%20Graham%20-%20LOVE%20SOMEONE%20%28Cover%20by%20Leroy%20Sanchez%29.mp3'
-          },
-          { 
-            id:4,
-            image: "https://www.scoutmag.ph/wp-content/uploads/2019/03/Screen-Shot-2019-03-19-at-9.18.13-AM.png", 
-            title:"Get You x Redbone", 
-            artist: "Daniel Caesar (JamieBoy Cover)", 
-            album: "JamieBoy", 
-            duration:"3:21", 
-            path: 'https://ia801502.us.archive.org/20/items/getyouxredbonedanielcaesarchildishgambinojamieboycover/Get%20You%20x%20Redbone%20-%20Daniel%20Caesar%20%26%20Childish%20Gambino%20%28JamieBoy%20Cover%29.mp3'
-          },
-          { 
-            id:5,
-            image: "https://www.hellomagazine.com/imagenes/celebrities/2017012336032/Ed-Sheeran-younger-doppelganger-music-video/0-194-667/ed-new-t.jpg", 
-            title:"Best Part Of Me", 
-            artist: "Ed Sheeran (feat. YEBBA)", 
-            album: "Ed Sheeran", 
-            duration:"4:08", 
-            path: 'https://ia801404.us.archive.org/17/items/edsheeranbestpartofmefeat.yebbaliveatabbeyroad/Ed%20Sheeran%20-%20Best%20Part%20Of%20Me%20%28feat.%20YEBBA%29%20%28Live%20At%20Abbey%20Road%29.mp3'
-          },
-          { 
-            id:6,
-            image: "https://i.scdn.co/image/f55cab0739390cf3b2c2f773b9c779b2f0ae8a99", 
-            title:"Thursday", 
-            artist: "Jess Glynne x Ed Sheeran", 
-            album: "Ed Sheeran", 
-            duration:"4:08", 
-            path: 'https://ia601508.us.archive.org/18/items/jessglynnexedsheeranthursdayacoustic/Jess%20Glynne%20x%20Ed%20Sheeran%20-%20Thursday%20%5BAcoustic%5D.mp3'
-          },
-          { 
-            id:7,
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDxS-GgJ-c5zQsXVMfl_UEfdzHg9Nryq-uSYbyQNuy2pF-HhvXOQ", 
-            title:"Your Song", 
-            artist: "Rita Ora ft Ed Sheeran", 
-            album: "Ed Sheeran", 
-            duration:"4:08", 
-            path: 'https://ia801406.us.archive.org/16/items/ritaorayoursongft.edsheeran/Rita%20OraYour%20Song%20%28ft.%20Ed%20Sheeran%29.mp3'
-          },
-          { 
-            id:8,
-            image: "https://i.guim.co.uk/img/media/ea014c51de0a5c96fac2a1ef939c36e6a4a6b5e7/0_181_1448_1810/master/1448.jpg?width=300&quality=85&auto=format&fit=max&s=100eb26d484ea20742eac971a6a5c0a0", 
-            title:"Beautiful People", 
-            artist: "Ed Sheeran", 
-            album: "Ed Sheeran", 
-            duration:"4:08", 
-            path: 'https://ia801501.us.archive.org/4/items/edsheeranbeautifulpeopleliveatabbeyroad1/Ed%20Sheeran%20-%20Beautiful%20People%20%28Live%20At%20Abbey%20Road%29%20%281%29.mp3'
-          },
+          // { 
+          //   id: 'ia601407',
+          //   image: "http://www.fouroverfour.jukely.com/wp-content/uploads/2017/01/Leroy-Sanchez.jpg", 
+          //   title:"Never Really Over", 
+          //   artist: "Cover by Leroy Sanchez", 
+          //   album: "Leroy Sanchez", 
+          //   duration:"4:29", 
+          //   path: 'https://ia601407.us.archive.org/11/items/katyperryneverreallyovercoverbyleroysanchez/KATY%20PERRY%20-%20Never%20Really%20Over%20%28Cover%20by%20Leroy%20Sanchez%29.mp3'
+          // },
+          // { 
+          //   id:'ia601408',
+          //   image: "http://www.fouroverfour.jukely.com/wp-content/uploads/2017/01/Leroy-Sanchez.jpg", 
+          //   title:"If I Can't Have You", 
+          //   artist: "Cover by Leroy Sanchez", 
+          //   album: "Leroy Sanchez", 
+          //   duration:"3:50", 
+          //   path: 'https://ia601507.us.archive.org/2/items/shawnmendesificanthaveyoucoverbyleroysanchez/SHAWN%20MENDES%20-%20If%20I%20Can%27t%20Have%20You%20%28Cover%20by%20Leroy%20Sanchez%29.mp3'
+          // },
+          // { 
+          //   id: 'ia801504',
+          //   image: "https://i1.sndcdn.com/avatars-000520361715-4dr7nr-t500x500.jpg", 
+          //   title:"Love Someone", 
+          //   artist: "Cover by Leroy Sanchez", 
+          //   album: "Leroy Sanchez", 
+          //   duration:"3:08", 
+          //   path: 'https://ia801504.us.archive.org/10/items/lukasgrahamlovesomeonecoverbyleroysanchez/Lukas%20Graham%20-%20LOVE%20SOMEONE%20%28Cover%20by%20Leroy%20Sanchez%29.mp3'
+          // },
+          // { 
+          //   id:4,
+          //   image: "https://www.scoutmag.ph/wp-content/uploads/2019/03/Screen-Shot-2019-03-19-at-9.18.13-AM.png", 
+          //   title:"Get You x Redbone", 
+          //   artist: "Daniel Caesar (JamieBoy Cover)", 
+          //   album: "JamieBoy", 
+          //   duration:"3:21", 
+          //   path: 'https://ia801502.us.archive.org/20/items/getyouxredbonedanielcaesarchildishgambinojamieboycover/Get%20You%20x%20Redbone%20-%20Daniel%20Caesar%20%26%20Childish%20Gambino%20%28JamieBoy%20Cover%29.mp3'
+          // },
+          // { 
+          //   id:5,
+          //   image: "https://www.hellomagazine.com/imagenes/celebrities/2017012336032/Ed-Sheeran-younger-doppelganger-music-video/0-194-667/ed-new-t.jpg", 
+          //   title:"Best Part Of Me", 
+          //   artist: "Ed Sheeran (feat. YEBBA)", 
+          //   album: "Ed Sheeran", 
+          //   duration:"4:08", 
+          //   path: 'https://ia801404.us.archive.org/17/items/edsheeranbestpartofmefeat.yebbaliveatabbeyroad/Ed%20Sheeran%20-%20Best%20Part%20Of%20Me%20%28feat.%20YEBBA%29%20%28Live%20At%20Abbey%20Road%29.mp3'
+          // },
+          // { 
+          //   id:6,
+          //   image: "https://i.scdn.co/image/f55cab0739390cf3b2c2f773b9c779b2f0ae8a99", 
+          //   title:"Thursday", 
+          //   artist: "Jess Glynne x Ed Sheeran", 
+          //   album: "Ed Sheeran", 
+          //   duration:"4:08", 
+          //   path: 'https://ia601508.us.archive.org/18/items/jessglynnexedsheeranthursdayacoustic/Jess%20Glynne%20x%20Ed%20Sheeran%20-%20Thursday%20%5BAcoustic%5D.mp3'
+          // },
+          // { 
+          //   id:7,
+          //   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDxS-GgJ-c5zQsXVMfl_UEfdzHg9Nryq-uSYbyQNuy2pF-HhvXOQ", 
+          //   title:"Your Song", 
+          //   artist: "Rita Ora ft Ed Sheeran", 
+          //   album: "Ed Sheeran", 
+          //   duration:"4:08", 
+          //   path: 'https://ia801406.us.archive.org/16/items/ritaorayoursongft.edsheeran/Rita%20OraYour%20Song%20%28ft.%20Ed%20Sheeran%29.mp3'
+          // },
+          // { 
+          //   id:8,
+          //   image: "https://i.guim.co.uk/img/media/ea014c51de0a5c96fac2a1ef939c36e6a4a6b5e7/0_181_1448_1810/master/1448.jpg?width=300&quality=85&auto=format&fit=max&s=100eb26d484ea20742eac971a6a5c0a0", 
+          //   title:"Beautiful People", 
+          //   artist: "Ed Sheeran", 
+          //   album: "Ed Sheeran", 
+          //   duration:"4:08", 
+          //   path: 'https://ia801501.us.archive.org/4/items/edsheeranbeautifulpeopleliveatabbeyroad1/Ed%20Sheeran%20-%20Beautiful%20People%20%28Live%20At%20Abbey%20Road%29%20%281%29.mp3'
+          // },
           { 
               id:9,
               image: "https://i.scdn.co/image/f55cab0739390cf3b2c2f773b9c779b2f0ae8a99", 
